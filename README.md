0707aa362cfce94db8a112d74df6390ba683e828
Justin Schuhmann <jmschu02@gmail.com>
2022-05-31 18:56:50 -0400

    Initial commit - Added a list of all students

---
+++ README.md
@@ -0,0 +1,3 @@
+# Code Louisville Python List Demo
+
+This is a demo of the presentation given on 5/31/2022
---
+++ example.py
@@ -0,0 +1,32 @@
+students = [
+    {
+        "fname": "Adrienne",
+        "lname": "Adams"
+    },
+    {
+        "fname": "Doug",
+        "lname": "Wiley"
+    },
+    {
+        "fname": "Emily",
+        "lname": "Coleman"
+    },
+    {
+        "fname": "Grace",
+        "lname": "Ensign"
+    },
+    {
+        "fname": "Katie",
+        "lname": "Cook"
+    },
+    {
+        "fname": "Kimberly",
+        "lname": "B"
+    },
+    {
+        "fname": "Mariah",
+        "lname": "Corso"
+    }
+]
+
+print(students)
