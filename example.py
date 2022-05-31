769cdd0f6b8c800e0a637dfb643fe47fafa112ed
Justin Schuhmann <jmschu02@gmail.com>
2022-05-31 19:27:46 -0400

    Add functions and enabled modular use

--- example.py
+++ example.py
@@ -1,37 +1,51 @@
-students = [
-    {
-        "fname": "Adrienne",
-        "lname": "Adams"
-    },
-    {
-        "fname": "Doug",
-        "lname": "Wiley"
-    },
-    {
-        "fname": "Emily",
-        "lname": "Coleman"
-    },
-    {
-        "fname": "Grace",
-        "lname": "Ensign"
-    },
-    {
-        "fname": "Katie",
-        "lname": "Cook"
-    },
-    {
-        "fname": "Kimberly",
-        "lname": "B"
-    },
-    {
-        "fname": "Mariah",
-        "lname": "Corso"
-    }
-]
+def load_students():
+    students = [
+        {
+            "fname": "Adrienne",
+            "lname": "Adams"
+        },
+        {
+            "fname": "Doug",
+            "lname": "Wiley"
+        },
+        {
+            "fname": "Emily",
+            "lname": "Coleman"
+        },
+        {
+            "fname": "Grace",
+            "lname": "Ensign"
+        },
+        {
+            "fname": "Katie",
+            "lname": "Cook"
+        },
+        {
+            "fname": "Kimberly",
+            "lname": "B"
+        },
+        {
+            "fname": "Mariah",
+            "lname": "Corso"
+        }
+    ]
 
-for student in students:
-    print("Hello " + student["fname"])
-    input("What did you work on last week?")
-    input("What are you working on this week?")
-    input("What do you need help with?")
-    print(f"Thanks so much {student['lname']}")
+    return students
+
+
+def standup(students):
+    for student in students:
+        print("Hello " + student["fname"])
+        input("What did you work on last week?")
+        input("What are you working on this week?")
+        input("What do you need help with?")
+        print(f"Thanks so much {student['lname']}")
+
+
+def main():
+    students = load_students()
+    standup(students)
+
+
+if __name__ == "__main__":
+    main()
